8720a0aea4cbf00c919f047a1b03449f07fa0e7e
Marley Alford <marleycalford@gmail.com>
2026-03-07 17:04:17 +0100

    fix: apply extension to avoid urls only

--- background/background.js
+++ background/background.js
@@ -1,29 +1,40 @@
 // listen to page navigation and respond accordingly
 
+function toMatchPattern(url) {
+	try {
+			const { protocol, hostname } = new URL(url);
+			return `${protocol}//${hostname}/*`;
+	} catch {
+			return null;
+	}
+}
+
 function registerListener(urls) {
-	console.log('add listener');
-	console.log(urls);
 	// Remove the old listener before adding a new one
 	if (chrome.webRequest.onBeforeRequest.hasListener(callback)) {
 		chrome.webRequest.onBeforeRequest.removeListener(callback);
 	}
+
+	const patterns = urls.map(toMatchPattern).filter(Boolean); // checks that url is parsable
+  if (patterns.length === 0) return; // return if url not parsable
 	
 	chrome.webRequest.onBeforeRequest.addListener(
 		callback,
-		{ urls: urls }
+		{ urls: patterns }
 	);
 }
 
 function callback(details) {
 	if (details.tabId === -1) return;
-	console.log('callback')
 
+	chrome.scripting.insertCSS({
+		target: { tabId: details.tabId },
+		files: ["content/content.css"]
+	});
+	
 	chrome.scripting.executeScript({
 		target: { tabId: details.tabId },
-		func: (url) => {
-			window.alert(`Are you sure you want to visit ${url}?`);
-		},
-		args: [details.url]
+		files: ["content/content.js"]
 	});
 }
 
--- content/content.js
+++ content/content.js
@@ -1,1 +1,20 @@
-// todo
+function showOverlay () {
+    // Don't inject twice
+    if (document.getElementById("foqus-overlay")) return;
+
+    // Create the overlay element
+    const overlay = document.createElement("div");
+    overlay.className = "foqus-overlay";
+
+    // Add content
+    const title = document.createElement("h1");
+    title.innerHTML = "Attempting to visit guilty pleasure site";
+    title.style.fontSize = "50px";
+    title.style.color = "white";
+    overlay.appendChild(title);
+
+    // Inject into page
+    document.body.appendChild(overlay);
+}
+
+showOverlay();
